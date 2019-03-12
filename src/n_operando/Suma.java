24f7f6766bb6739dbf4671b49a796e59ae091d79
Samuel <svcsamu@gmail.com>
2019-03-12 12:22:30 +0100

    Commit Suma2

--- src/n_operando/Suma.java
+++ src/n_operando/Suma.java
@@ -4,4 +4,7 @@ public class Suma {
 	public static void main (String[]args) {
 		System.out.println("Vamos a Sumar .");
 	}
+	public void suma (int a, int b) {
+		System.out.println("El resultado de la suma es: "+(a+b));
+	}
 }
